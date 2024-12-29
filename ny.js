822cd7f0faa0c35e09e96bd0674663a6934b5ddc
Ian3556 <112095786+Ian3556@users.noreply.github.com>
2024-12-29 22:34:55 +0800

    Add files via upload

---
+++ ny.js
@@ -0,0 +1,32 @@
+function shine() {
+    const count = 1000;
+    const section = document.querySelector("section");
+    var i = 0;
+    while (i < count) {
+      const star = document.createElement("i");
+      const x = Math.floor(Math.random() * window.innerWidth);
+      const y = Math.floor(Math.random() * window.innerHeight);
+  
+      const size = Math.random() * 4;
+      star.style.left = x + "px";
+      star.style.top = y + "px";
+      star.style.width = 2 + size + "px";
+      star.style.height = 2 + size + "px";
+  
+      const duration = Math.random() * 2;
+      star.style.animationDuration = 2 + duration + "s";
+      star.style.animationDelay = duration + "s";
+  
+      section.appendChild(star);
+      i++;
+  
+      // Remove the star after the animation ends
+      setTimeout(() => {
+        star.remove();
+      }, (2 + duration) * 1000);
+    }
+  }
+  
+  shine();
+  
+
